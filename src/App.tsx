038394ca514b7a16cf05984fdb694a78be67243e
Shohrukh Qurbonov <shohrukh.m.qurbonov@gmail.com>
2025-11-30 16:15:56 +0500

    fixed project structur

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,34 @@
-import { Advantage } from "./pages/advantages/advantage";
-import { Projects } from "./pages/components/projects/projects";
-import { Computer } from "./pages/computers/computers";
-import { Footer } from "./pages/footer/footer";
-import { Header } from "./pages/header/header";
-import { Hero } from "./pages/hero/hero";
-import { News } from "./pages/news/news";
-import { GetStarted } from "./pages/started/started";
-import { Technology } from "./pages/technology/technology";
-import Testimonials from "./pages/users/users";
-import styles from "../src/style/global.module.css"
+import styles from "../src/style/global.module.css";
+import { Advantage } from "./module/advantages/advantage";
+import { Projects } from "./module/components/projects/projects";
+import { Computer } from "./module/computers/computers";
+import { Footer } from "./module/footer/footer";
+import { Header } from "./module/header/header";
+import { Hero } from "./module/hero/hero";
+import { News } from "./module/news/news";
+import { GetStarted } from "./module/started/started";
+import { Technology } from "./module/technology/technology";
+import Testimonials from "./module/users/users";
 // import { Route, Routes } from "react-router-dom";
 
 function App() {
-    return (
-        <div className={styles.root}>
-            <Header/>
-            <Hero/>
-            <Projects/>
-            <Advantage/>
-            <Computer/>
-            <Technology/>
-            <Testimonials/>
-            <GetStarted/>
-            <News/>
-            <Footer/>
-            {/* <Routes>
+  return (
+    <div className={styles.root}>
+      <Header />
+      <Hero />
+      <Projects />
+      <Advantage />
+      <Computer />
+      <Technology />
+      <Testimonials />
+      <GetStarted />
+      <News />
+      <Footer />
+      {/* <Routes>
                 <Route path="/" element></Route>
             </Routes> */}
-        </div>
-    )
+    </div>
+  );
 }
 
-export default App
+export default App;
